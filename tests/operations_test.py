f4e257a5cf2e89252e6c8f05498e67124caf76b1
Ishan Pant <91046758+Ishan1Pant@users.noreply.github.com>
2024-09-04 17:15:19 +0530

    Update operations_test.py

---
+++ tests/operations_test.py
@@ -0,0 +1,11 @@
+from src.math_operations import add,sub 
+
+def test_add():
+    assert add(2,3)==5 
+    assert add(-1,1)==0 
+    assert add(5,5)==10 
+
+def test_sun():
+    assert sub(5,2)==3
+    assert sub(8,3)==5 
+    assert sub(-5,3)==-8 
